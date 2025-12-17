88296598c3361232dd193d83ecc3eeba35a7c7d7
Marwan Abdelhamid <101995294+marwannelsayed@users.noreply.github.com>
2025-12-17 23:18:48 +0100

    changes in the astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  site: 'https://myabdelhamid.github.io',
 });
